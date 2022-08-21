b76a704fd49503c9ba16a32e7d1f3eac2f67c078
shindeAkshu <sakshata416@gmail.com>
2022-08-21 11:11:15 -0700

    Responsive-App

--- src/App.js
+++ src/App.js
@@ -5,10 +5,14 @@ import './App.css';
 
 function App() {
   const[items,setItems] = useState();
-  
+
   return (
     <div className="App">
-     
+     <div className="Container">
+      <div className="navBar">
+        <a href="">Home</a>
+      </div>
+     </div>
     </div>
   );
 }
