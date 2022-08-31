d8ed9afceba0f0287a1c20b3b83ce45b86cddb17
shindeAkshu <sakshata416@gmail.com>
2022-08-31 11:03:19 -0700

    Responsive-App

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {useState} from 'react';
 
 function Navbar() {
+    const[navitems,setNavItems] = useState();
     return(
         <nav>
             <span>Home</span>
