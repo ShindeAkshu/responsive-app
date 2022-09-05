473e84e0b688132a5298b635955f3cf7ccd2ae69
shindeAkshu <sakshata416@gmail.com>
2022-09-05 11:12:10 -0700

    Responsive-App

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,7 +5,12 @@ import {link} from 'react-router-dom';
 function Navbar() {
     return(
         <nav className='navbar'>
-          
+            <div className='navbar-container'>
+                <link to="/" className='navbar-logo'>
+                    Travels <i className="fab fa-typo3"></i>
+                </link>
+
+            </div>
         </nav>
     ) 
 
