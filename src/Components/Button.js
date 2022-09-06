15e7f8a27ee29a42d2d175f60d32df64b94c86fc
shindeAkshu <sakshata416@gmail.com>
2022-09-05 20:47:22 -0700

    Responsive-App

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -4,12 +4,3 @@ import {Link} from'react-router-dom';
 
 const STYLES=['btn--primary' , 'btn--outline']
 
-const SIZES = ['btn-medium','btn-large']
- 
-export default Button= ({childern,
-    type,
-    onClick,
-    butttonStyle,
-    buttonSize}) =>{
-      const checkButtonStyle = STYLES.includes (buttonStyle) ? buttonStyle : STYLES[0]
-    };
