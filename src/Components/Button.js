2dc0719d6188baeba50c8a18a580fc74f5745c71
shindeAkshu <sakshata416@gmail.com>
2022-09-07 11:25:53 -0700

    Responsive-App

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -7,5 +7,9 @@ const STYLES=['btn--primary' , 'btn--outline']
 const SIZES = ['btn-medium','btn-large']
  
 export default Button= ({childern,
-
+    type,
+    onClick,
+    butttonStyle,
+    buttonSize}) =>{
+      const checkButtonStyle = STYLES.includes (buttonStyle) ? buttonStyle : STYLES[0]
     };
