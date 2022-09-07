4ca28b7404ca57fa25700189385802cce5d764b1
shindeAkshu <sakshata416@gmail.com>
2022-09-07 11:23:57 -0700

    Responsive-App

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -19,7 +19,26 @@ function Navbar() {
                     <i className= {click ? 'fas fa-times':'fas fa-bars'} />
                 </div>
                 <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                   
+                    <li className='nav-item'>
+                        <link to='/' className='nav-links' onClick={closeMobileMenu}>
+                            Home
+                        </link>
+                    </li>
+                    <li className='nav-item'>
+                        <link to='/services' className='nav-links' onClick={closeMobileMenu}>
+                            Services
+                        </link>
+                    </li>
+                    <li className='nav-item'>
+                        <link to='/product' className='nav-links' onClick={closeMobileMenu}>
+                            Product
+                        </link>
+                    </li>
+                    <li className='nav-item'>
+                        <link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
+                           Sign-Up 
+                        </link>
+                    </li>
 
                 </ul>
 
