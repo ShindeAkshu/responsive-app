6b2a5748c6bc6ce90ad687e61c8b1f5d7f7405b6
shindeAkshu <sakshata416@gmail.com>
2022-09-08 20:54:21 -0700

    Responsive-App

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -13,5 +13,20 @@ export default Button= ({childern,
     buttonSize}) =>{
       const checkButtonStyle = STYLES.includes (buttonStyle) ? buttonStyle : STYLES[0];
 
-      const checkButtonSize = SIZES.includes (buttonSize) ? buttonSize : SIZES[0]; 
+      const checkButtonSize = SIZES.includes (buttonSize) ? buttonSize : SIZES[0];
+      return(
+        <Link to='/sign-up' className='btn-mobile'>
+        <button
+        className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+        onClick={onClick}
+        type={type}>
+          {childern}
+        </button>
+
+        <button>
+          Get Started
+        </button>
+        </Link>
+      ) 
+
     };
