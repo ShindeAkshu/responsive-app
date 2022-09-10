a490e207ab888a8588551c23deae265614853d1e
shindeAkshu <sakshata416@gmail.com>
2022-09-10 11:18:17 -0700

    Updated File

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -14,19 +14,4 @@ export default Button= ({childern,
       const checkButtonStyle = STYLES.includes (buttonStyle) ? buttonStyle : STYLES[0];
 
       const checkButtonSize = SIZES.includes (buttonSize) ? buttonSize : SIZES[0];
-      return(
-        <Link to='/sign-up' className='btn-mobile'>
-        <button
-        className={`btn ${checkButtonStyle} ${checkButtonSize}`}
-        onClick={onClick}
-        type={type}>
-          {childern}
-        </button>
-
-        <button>
-          Get Started
-        </button>
-        </Link>
-      ) 
-
-    };
+     
