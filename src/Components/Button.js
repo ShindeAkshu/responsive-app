c69701b352809af4f746857d914b43feb4d0c297
shindeAkshu <sakshata416@gmail.com>
2022-09-12 11:10:16 -0700

    Responsive-App

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -4,7 +4,7 @@ import {Link} from'react-router-dom';
 
 const STYLES=['btn--primary' , 'btn--outline']
 
-const SIZES = ['btn-medium','btn-large']
+
  
 export default Button= ({childern,
     type,
